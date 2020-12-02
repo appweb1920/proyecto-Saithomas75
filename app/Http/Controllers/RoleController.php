c8e008df9e0ef5329b6c6f3008af7d6255cc638b
Saithomas75 <a279627@alumnos.uaslp.mx>
2020-12-02 17:28:55 -0600

    Metodos edit y update role

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -73,9 +73,17 @@ class RoleController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Role $role)
     {
-        //
+        $permissions_role = [];
+
+        foreach($role->permissions as $p){
+            $permissions_role[] = $p->id;
+        }
+
+        $permissions = Permission::get();
+
+        return view('role.edit', compact('permissions', 'role', 'permissions_role'));
     }
 
     /**
@@ -85,9 +93,21 @@ class RoleController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Role $role)
     {
-        //
+        $request->validate([
+            'name' => 'required|max:50|unique:roles,name,'.$role->id,
+            'slug' => 'required|max:50|unique:roles,slug,'.$role->slug,
+            'full-access' => 'required|in:yes,no'
+        ]);
+
+        $role->update($request->all());
+
+        if($request->get('permission')){
+            $role->permissions()->sync($request->get('permission'));
+        }
+
+        return redirect()->route('role.index')->with('status_success', 'Role update successfully');
     }
 
     /**
